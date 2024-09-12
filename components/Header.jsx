cfbdb52e2abfbbaed31119e1b44e8ae340afbacd
yamin <yamin148@infinity.gmail.com>
2024-09-12 16:45:07 +0700

    added font family for title

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import { FaBars, FaXmark  } from "react-icons/fa6";
 import styles from '@/styles/Header.module.css'
 import { MobileToggleContext } from '@/context/MobileToggleContext';
 
-export default function Header() {
+export default function Header({ customFont }) {
     const { toggle, setToggle } = useContext(MobileToggleContext);
 
     const handleChangeToggle = () => {
@@ -14,7 +14,7 @@ export default function Header() {
         <section className={styles.wrapper}>
             <div className={styles.container}>
                 <div>
-                    <h1 className={styles['left-text']}>Kaung <span className='text-primary'>&</span> Hanny</h1>
+                    <h1 className={`${styles['left-text']} ${customFont.className}`}>Kaung <span className='text-primary'>&</span> Hanny</h1>
                 </div>
                 <div className={styles['right-div']}>
                     <h1 className={styles['right-text']}>Saturday, 2 <span className='text-primary'>December</span> 2024</h1>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import localFont from "next/font/local";
+import { Playpen_Sans } from "next/font/google";
 import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
-import { useContext } from "react";
-import { MobileToggleContext } from "@/context/MobileToggleContext";
 import ToggleBar from "@/components/ToggleBar";
 
 const geistSans = localFont({
@@ -10,22 +9,25 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
 
-export default function Home() {
-  const { toggle } = useContext(MobileToggleContext);
+const playwrightCuba = Playpen_Sans({
+  variable: "--font-playwright-cuba",
+  subsets: ["latin"]
+});
 
+export default function Home() {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} flex flex-col items-center min-h-screen font-[family-name:var(--font-geist-sans)]`}
     >
-      <Header />
+      <Header customFont={playwrightCuba} />
       <NavBar />
-      {/* { toggle && <ToggleBar /> } */}
       <ToggleBar />
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>hello</h1>
