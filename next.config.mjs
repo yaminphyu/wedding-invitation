c21c05ba3503d8ff57091a64524770940773db7a
yamin <yamin148@infinity.gmail.com>
2024-09-30 12:23:48 +0700

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     APP_VERSION: process.env.APP_VERSION,
+    WEBSITE_NAME: process.env.WEBSITE_NAME,
     WEDDIND_DATE: process.env.WEDDIND_DATE,
     WEDDIND_TIME: process.env.WEDDIND_TIME,
     WEDDING_ADDRESS: process.env.WEDDING_ADDRESS,
