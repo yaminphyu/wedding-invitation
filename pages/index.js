ea52d12adf495aa6f5df0f5eede7091e20d66e94
myatnoe95 <khingsunthwe@gmail.com>
2024-10-07 00:23:31 +0700

    refactor: Refactor font imports and fix formatting in index.js

--- pages/index.js
+++ pages/index.js
@@ -14,50 +14,50 @@ import WeddingEvent from "@/components/WeddingEvent";
 import Head from "next/head";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: "./fonts/GeistVF.woff",
+	variable: "--font-geist-sans",
+	weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: "./fonts/GeistMonoVF.woff",
+	variable: "--font-geist-mono",
+	weight: "100 900",
 });
 
 const playwrightCuba = Playpen_Sans({
-  variable: "--font-playwright-cuba",
-  subsets: ["latin"]
+	variable: "--font-playwright-cuba",
+	subsets: ["latin"],
 });
 
 export default function Home() {
-  const { width } = useWindowDimension();
+	const { width } = useWindowDimension();
 
-  const { toggle, setToggle } = useContext(MobileToggleContext);
+	const { toggle, setToggle } = useContext(MobileToggleContext);
 
-  useEffect(() => {
-    if (width > 990 && toggle) {
-      setToggle(false);
-    }
-  }, [setToggle, toggle, width]);
-  
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} flex flex-col items-center min-h-screen font-[family-name:var(--font-geist-sans)]`}
-    >
-      <Head>
-        <title>{process.env.WEBSITE_NAME}</title>
-        <link rel="icon" href='favicon.jpg' />
-      </Head>
-      <Header customFont={playwrightCuba} />
-      <NavBar />
-      <ToggleBar />
-      <main className="w-full flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <HeroSection customFont={playwrightCuba} />
-        <OurDaySection customFont={playwrightCuba} />
-        <OurStorySection customFont={playwrightCuba} />
-        <GallerySection customFont={playwrightCuba} />
-        <WeddingEvent customFont={playwrightCuba} />
-      </main>
-    </div>
-  );
+	useEffect(() => {
+		if (width > 990 && toggle) {
+			setToggle(false);
+		}
+	}, [setToggle, toggle, width]);
+
+	return (
+		<div
+			className={`${geistSans.variable} ${geistMono.variable} flex flex-col items-center min-h-screen font-[family-name:var(--font-geist-sans)]`}
+		>
+			<Head>
+				<title>{process.env.WEBSITE_NAME}</title>
+				<link rel="icon" href="favicon.jpg" />
+			</Head>
+			<Header customFont={playwrightCuba} />
+			<NavBar />
+			<ToggleBar />
+			<main className="w-full flex flex-col gap-8 row-start-2 items-center sm:items-start">
+				<HeroSection customFont={playwrightCuba} />
+				<OurDaySection customFont={playwrightCuba} />
+				<OurStorySection customFont={playwrightCuba} />
+				<GallerySection customFont={playwrightCuba} />
+				<WeddingEvent customFont={playwrightCuba} />
+			</main>
+		</div>
+	);
 }
